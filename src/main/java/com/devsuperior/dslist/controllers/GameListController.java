0226d37bd0d73f4fd8c35f6856e469ce51511a9e
Otavio Chaves <otaviocs2005@gmail.com>
2024-11-14 21:33:49 -0300

    Sql query for search games by list type (fixed)

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -26,7 +26,7 @@ public class GameListController {
         return gameListService.findAllLists();
     }
 
-    @GetMapping(value = "/{listId}/")
+    @GetMapping(value = "/{listId}")
     public List<GameMinDTO> findByList(@PathVariable long listId){
         return gameService.findByList(listId);
     }
