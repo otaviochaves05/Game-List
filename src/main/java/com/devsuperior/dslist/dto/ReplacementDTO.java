acafd5252a7b0508bfda8335ac51ab456197086a
Otavio Chaves <otaviocs2005@gmail.com>
2024-11-18 17:18:22 -0300

    move games inside list

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+    private int sourceIndex;
+    private int destinationIndex;
+
+    public int getSourceIndex() {
+        return sourceIndex;
+    }
+
+    public void setSourceIndex(int sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+
+    public int getDestinationIndex() {
+        return destinationIndex;
+    }
+
+    public void setDestinationIndex(int destinationIndex) {
+        this.destinationIndex = destinationIndex;
+    }
+}
